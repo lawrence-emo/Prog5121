63108be2495837a9a01e1bd4cf6c1fe34c799ee8
lawrence-emo <166591475+lawrence-emo@users.noreply.github.com>
2024-04-16 10:51:48 +0200

    Add files via upload

---
+++ FullnameJava.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package fullname.java;
+
+/**
+ 
+ * @author RC_Student_lab
+ */
+public class FullnameJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String name ="Lawrence";
+        String number = "0710293879";
+        String email = "emolawrence10@gmail.com";
+        
+        System.out.println(name);
+        System.out.println(number);
+        System.out.println(email);
+        
+        
+    }
+    
+}
